9756bf4264aa5bd167c33f45a46298d655067da3
B Sai Umesh <saiumesh2003@gmail.com>
2025-11-15 22:06:18 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,28 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
+import SnippetCard from '../components/SnippetCard';
+import { toast } from 'react-toastify';
+
+export default function Home(){
+  const [snippets, setSnippets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    const load = async () => {
+      try {
+        const res = await api.get('/snippets');
+        setSnippets(res.data);
+      } catch (err) { toast.error('Failed to load'); }
+      setLoading(false);
+    }
+    load();
+  }, []);
+
+  if(loading) return <div>Loading...</div>;
+  return (
+    <div>
+      {snippets.map(s => <SnippetCard key={s._id} snippet={s} />)}
+    </div>
+  );
+}
 
