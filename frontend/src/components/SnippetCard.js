b7224215f4e130b972873bbd730ade0cd543ee93
B Sai Umesh <saiumesh2003@gmail.com>
2025-11-15 22:08:12 +0530

    Update SnippetCard.js

--- frontend/src/components/SnippetCard.js
+++ frontend/src/components/SnippetCard.js
@@ -1,1 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function SnippetCard({ snippet }){
+  return (
+    <div style={{ border:'1px solid #ddd', padding:12, margin:8 }}>
+      <h4><Link to={`/snippet/${snippet._id}`}>{snippet.title}</Link></h4>
+      <div><small>{snippet.language} • {snippet.tags?.join(', ')}</small></div>
+      <pre style={{ maxHeight:120, overflow:'hidden' }}>{snippet.code}</pre>
+    </div>
+  );
+}
 
